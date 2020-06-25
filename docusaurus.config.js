bd10151b71aa5e123d53dff274245a61e5d0d511
cathe-zhang <zhangle_media@hotmail.com>
2020-06-25 17:28:28 +0800

    conf: 修改首页标题

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
+  title: 'cathe-zhang',
+  tagline: 'cathe-zhang\'s home',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   favicon: 'img/favicon.ico',
@@ -8,7 +8,7 @@ module.exports = {
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'Home',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
