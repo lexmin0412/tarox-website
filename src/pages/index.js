100e9f913e9a3aeb7ab43e6062e197a90f2ecf3b
lexmin0412 <zhangle_media@hotmail.com>
2021-02-10 01:24:06 +0800

    refactor: 框架升级配置兼容

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,7 +68,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/')}>
+              to={useBaseUrl('docs/taro_002')}>
               前往
             </Link>
           </div>
